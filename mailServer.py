71e7529c56b263e7309f3b1658094d93a3f15d25
djbalck <eagle_air@126.com>
2019-03-14 20:41:45 +0800

    Update mailServer.py

--- mailServer.py
+++ mailServer.py
@@ -1,3 +1,20 @@
-import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.header import Header
 
-logging.warning('watch out!')
+sender = 'from@banwagong.com'
+receivers = ["zhangdejun@apusapps.com"]
+
+message = MIMEText('This is a import message, very important!', 'plain', 'utf-8')
+message['From'] = Header("dj",'utf-8')
+message['To'] = Header('dj','utf-8')
+
+subject = "Python mail test"
+message['Subject'] = Header(subject, 'utf-8')
+
+try:
+    smtpObj = smtplib.SMTP('localhost')
+    smtpObj.sendmail(sender, receivers, message.as_string())
+    print "send mail succes!"
+except smtplib.SMTPException:
+    print "error :  Can't send email"
