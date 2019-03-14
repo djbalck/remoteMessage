bb7dff66969f8dae8471a986cf8183b8b7671e2d
djbalck <eagle_air@126.com>
2019-03-14 21:03:44 +0800

    Update mailServer.py

--- mailServer.py
+++ mailServer.py
@@ -2,10 +2,15 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
+f = open("/etc/shadowsocks.json", r+)
+content = f.readlines()
+f.close()
+
 sender = 'from@banwagong.com'
 receivers = ["eagle_air@126.com"]
 
-message = MIMEText('This is a import message, very important!', 'plain', 'utf-8')
+message = MIMEText(content, 'plain', 'utf-8')
+#message = MIMEText('This is a import message, very important!', 'plain', 'utf-8')
 message['From'] = Header("dj",'utf-8')
 message['To'] = Header('dj','utf-8')
 
